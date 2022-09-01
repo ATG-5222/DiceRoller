c7834fdfcc95ca19b3de9631f9449253eafadd6d
ATG-5222 <A01275222@tec.mx>
2022-09-01 00:03:09 -0500

    Lanzamiento con imagenes

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,12 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Busqueda del textView
-        val rollButton: Button = findViewById(R.id.button2)
+        //Busqueda del boton
+        val rollButton: Button = findViewById(R.id.button3)
         //Listener, cuando se clickea el boton se ejecuta la función rollDice()
         rollButton.setOnClickListener { rollDice() }
-
-
+        //Lanzamiento del dado cuando se abre la aplicación
+        rollDice()
     }////////////////////////////////////////////////////////////////////////////////////
 
     private fun rollDice() { // Función para obtener un número aleatorio del dado
@@ -23,8 +24,18 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         //Actualización del valor de textView
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView3)
+        //Selección de la imagen correcta
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        //Actualización de la imagen
+        diceImage.setImageResource(drawableResource)
     }
 
 }////////////////////////////////////////////////////////////////////////////////////////
